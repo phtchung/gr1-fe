15389d32655d37205e62a45133b8f9c0ec22c4a9
phtchung <phamthanhchung2001@gmail.com>
2023-07-28 09:38:02 +0700

    add page task list ( ui + logic )

--- src/pages/tasklist/index.js
+++ src/pages/tasklist/index.js
@@ -1,16 +1,16 @@
 import React, {useState} from 'react';
 import HeaderLogin from "../../components/header-login";
 import SidebarUser from "../../components/sidebar";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import './tasklist.css'
-import {Button} from "flowbite-react";
-import {Pagination} from "@mui/material";
+import Item from "../../components/item";
+import {toDoTaskList, inProgressTaskList, doneTaskList} from "../../utils/constant";
+
 
 const TaskList = () => {
     const navigateName = 'tasklist'
-    const [value, setValue] = useState(new Date());
+    // const [value, setValue] = useState(new Date());
+
+
 
 
     return (
@@ -23,52 +23,51 @@ const TaskList = () => {
 
                 <div className="col col-9">
                     <div className="tasklist-wrap">
-                       <div className="publicsans-semi-bold-charade-18px tablename">
-                           Search
+                       <div className="publicsans-semi-bold-charade-18px tablename mb-md-5">
+                           TodoList
                        </div>
 
-                        <div className="search-tab">
-                            <div>
-                                <div className="dropdown">
-                                    <button className="btn btn-outline-dark  dropdown-toggle drop-search" type="button"
-                                            id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                        State
-                                    </button>
-                                    <ul className="dropdown-menu " style={{cursor:'pointer'}} aria-labelledby="dropdownMenuButton1">
-                                        <li><a className="dropdown-item" >Done</a></li>
-                                        <li><a className="dropdown-item" >Todo</a></li>
-                                        <li><a className="dropdown-item">Inprogress </a></li>
-                                    </ul>
+                        <div className="row list-wrap">
+                            <div className="col col-4 ">
+                                <div className="list-todo background-todo scrollable-col">
+                                    <div className="list-header background-todo header-sticky">
+                                        <span className="publicsans-semi-bold-charade-14px-overview " style={{'font-size':'18px'}}>Todo</span>
+                                        <span className="publicsans-semi-bold-charade-14px font-bold " style={{'font-size':'18px'}}>{toDoTaskList.length}</span>
+                                    </div>
+                                    {toDoTaskList.map((todoList, index) => (
+                                        <Item key={index} data={todoList} />
+                                    ))}
+
                                 </div>
-
-                            </div>
-                            <div>
-                                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                    <DatePicker
-
-                                        onChange={(newValue) => setValue(newValue)}
-                                    />
-                                </LocalizationProvider>
-                            </div>
-                            <div>
-                                <Button variant="contained"  className='btn-project btn-height'>
-                                    <span className="text-xl publicsans-semi-bold-charade-15px text-white ml-2 ">Search</span>
-                                </Button>
                             </div>
 
-                        </div>
+                            <div className="col col-4 ">
+                                <div className="list-todo background-inpro scrollable-col">
+                                    <div className="list-header background-inpro header-sticky">
+                                        <span className="publicsans-semi-bold-charade-14px-overview " style={{'font-size':'18px'}}>Inprogress</span>
+                                        <span className="publicsans-semi-bold-charade-14px font-bold " style={{'font-size':'18px'}}>{inProgressTaskList.length}</span>
+                                    </div>
+                                    {inProgressTaskList.map((inProgressList, index) => (
+                                        <Item key={index} data={inProgressList} />
+                                    ))}
+                                </div>
+                            </div>
 
-                    {/*    table*/}
-
-                        <div className="offset-4">
-                            <Pagination count={10}
-                                        // page={page} onChange={handleChange}
-                            />
+                            <div className="col col-4 ">
+                                <div className="list-todo background-done scrollable-col" >
+                                    <div className="list-header background-done header-sticky">
+                                        <span className="publicsans-semi-bold-charade-14px-overview " style={{'font-size':'18px'}}>Done</span>
+                                        <span className="publicsans-semi-bold-charade-14px font-bold " style={{'font-size':'18px'}}>{doneTaskList.length}</span>
+                                    </div>
+                                    {doneTaskList.map((doneList, index) => (
+                                        <Item key={index} data={doneList} />
+                                    ))}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
 
         </div>
     )
