965f5b239c3b9564febc3315a979bb62d3b7d0f9
phtchung <phamthanhchung2001@gmail.com>
2023-08-17 20:08:36 +0700

    api for task

---
+++ src/services/taskService.js
@@ -0,0 +1,125 @@
+import publicHttp from "./http/publicHttp.config";
+
+
+export const getTaskToday = async (userId) => {
+    return publicHttp({
+        method: 'GET',
+        url: `/overview/${userId}`,
+    })
+}
+
+
+export const getTaskTodo = async (userId) => {
+    return publicHttp({
+        method: 'GET',
+        url: `/todo/${userId}`,
+    })
+}
+
+export const getTaskInprogress = async (userId) => {
+    return publicHttp({
+        method: 'GET',
+        url: `/inprogress/${userId}`,
+    })
+}
+
+export const getTaskDone = async (userId) => {
+    return publicHttp({
+        method: 'GET',
+        url: `/done/${userId}`,
+    })
+}
+
+export const getSearchTask = async (userId) => {
+    return publicHttp({
+        method: 'GET',
+        url: `tasks/${userId}`,
+    })
+}
+
+export const addTask = async (task) => {
+    return publicHttp({
+        method: 'POST',
+        url: '/create_task',
+        data:
+            task
+
+    })
+}
+
+export const getDetailTask = async (taskId) => {
+    return publicHttp({
+        method: 'GET',
+        url: `task/${taskId}`,
+    })
+}
+
+
+export const getCheckList = async (taskId) => {
+    return publicHttp({
+        method: 'GET',
+        url: `task/checklist/${taskId}`,
+    })
+}
+
+export const createCheckList = async(checklist) => {
+    return publicHttp({
+        method : 'POST',
+        url : '/task/checklist',
+        data : checklist
+    })
+}
+
+export const ShareTask = async (taskId, shareData) => {
+    return publicHttp({
+        method: 'POST',
+        url: `task/share/${taskId}`,
+        data: shareData
+    })
+}
+
+export const removeCheckList = async ({checkListId}) => {
+    return publicHttp({
+        method: 'DELETE',
+        url: '/task/checklist',
+        data:{
+            checkListId
+        },
+
+    })
+}
+export const removeTask = async ({taskId}) => {
+    return publicHttp({
+        method: 'DELETE',
+        url: '/remove_task',
+        data:
+            {taskId},
+
+
+    })
+}
+
+export const updateUserInfo = async (data) => {
+    return publicHttp({
+        method: 'PUT',
+        url: '/user',
+        data
+    })
+}
+
+
+export const updateTaskInfo = async (data) => {
+    return publicHttp({
+        method: 'PUT',
+        url: '/task/update',
+        data
+    })
+}
+
+export const updateCheckListInfo = async (data) => {
+    return publicHttp({
+        method: 'PUT',
+        url: '/task/checklist/update',
+        data
+    })
+}
